bc37731875fe971f5de87604291335cd14883035
AodaCat <pczqiang@gmail.com>
2018-09-17 21:32:11 +0800

    2.11 Exercise 6 (2)

--- Exercise.java
+++ Exercise.java
@@ -1,20 +1,64 @@
 // 2.11 
 
-// Exercise 1
-public class Exercise{
+/**
+ * Exercise 1 (2)
+ */
+// public class Exercise{
+//     int a;
+//     char b;
+//     public Exercise(){
+//         System.out.println("a = " + a);
+//         System.out.println("b = " + b);
+//     }
+//     public static void main(String[] args) {
+//         new Exercise();
+//     }
+// }
 
-    int a;
-    char b;
+/**
+ * Exercise 2 (1)
+ */
+// public class Exercise{
+//     public static void main(String[] args){
+//         System.out.println("Hello, World");
+//     }
+// }
 
-    public Exercise(){
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-    }
+/**
+ * Exercise 5 (1)
+ */
+// public class Exercise{
+//     int i;
+//     double d;
+//     boolean b;
+//     public void printData(){
+//         System.out.println(i);
+//         System.out.println(d);
+//         System.out.println(b);
+//     }
+//     public static void main(String[] args){
+//         Exercise e = new Exercise();
+//         e.printData();
+//         e.b = true;
+//         e.d = 1.1;
+//         e.i = 1;
+//         e.printData();
+//     }
+// }
 
-    public static void main(String[] args) {
-        new Exercise();
-
-    }
-
-}
-
+/**
+ * Exercise 6 (2)
+ */
+// public class Exercise{
+//     String s = "String 666";
+//     public int slength(){
+//         return s.length();
+//     }
+//     public void sprint(){
+//         System.out.println(slength());
+//     }
+//     public static void main(String[] arga){
+//         Exercise e = new Exercise();
+//         e.sprint();
+//     }
+// }
