3f38813a1a559b09586ccad3bbdc7f390e8dd65d
AodaCat <pczqiang@gmail.com>
2018-09-17 20:00:28 +0800

    creat exercise

---
+++ Exercise.java
@@ -0,0 +1,20 @@
+// 2.11 
+
+// Exercise 1
+public class Exercise{
+
+    int a;
+    char b;
+
+    public Exercise(){
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+    public static void main(String[] args) {
+        new Exercise();
+
+    }
+
+}
+
